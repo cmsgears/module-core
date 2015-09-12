7b48d2bd85c3555bb3f0fa079266c9b95c9712d6
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-09-12 18:19:31 +0530

    Added field for video.

--- common/config/CoreGlobal.php
+++ common/config/CoreGlobal.php
@@ -133,6 +133,7 @@ class CoreGlobal {
 	const FIELD_TAG					= 'tagField';
 	const FIELD_AVATAR				= 'avatarField';
 	const FIELD_BANNER				= 'bannerField';
+	const FIELD_VIDEO				= 'videoField';
 	const FIELD_USER				= 'userField';
 	const FIELD_AUTHOR				= 'authorField';
 	const FIELD_MEMBER				= 'memberField';

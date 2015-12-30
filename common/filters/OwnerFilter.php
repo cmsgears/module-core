8a62829530a903190a4443d8d5f117444deef422
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-12-30 20:32:16 +0530

    Added owner filter.

---
+++ common/filters/OwnerFilter.php
@@ -0,0 +1,32 @@
+<?php
+namespace cmsgears\core\common\filters;
+
+// Yii Imports
+use Yii;
+use yii\web\ForbiddenHttpException;
+
+// CMG Imports
+use cmsgears\core\common\config\CoreGlobal;
+
+class OwnerFilter {
+
+	public function doFilter( $args = [] ) {
+
+		$user	= Yii::$app->user->identity;
+		
+		if( isset( $args[ 'slug' ] ) && $args[ 'slug' ] ) {
+			
+			$modelService	= Yii::createObject( $args[ 'service' ] );
+			$slug			= Yii::$app->request->get( 'slug' );
+			$model			= $modelService::findBySlug( $slug );
+
+			if( !$model->isOwner( $user ) ) {
+
+				// Not Allowed
+				throw new ForbiddenHttpException( Yii::$app->cmgCoreMessage->getMessage( CoreGlobal::ERROR_NOT_ALLOWED ) );
+			}
+		}
+	}
+}
+
+?>

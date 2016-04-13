6b22db503595955de5c843a97c78569d7f16a279
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-04-13 07:49:39 +0530

    Changed site member package.

---
+++ admin/services/mappers/SiteMemberService.php
@@ -0,0 +1,17 @@
+<?php
+namespace cmsgears\core\admin\services\mappers;
+
+// Yii Imports
+use \Yii;
+
+// CMG Imports
+use cmsgears\core\common\models\entities\Site;
+use cmsgears\core\common\models\mappers\SiteMember;
+
+class SiteMemberService extends \cmsgears\core\common\services\mappers\SiteMemberService {
+
+	// Static Methods ----------------------------------------------
+
+}
+
+?>

d948c55954d001e65b3aa60b4e2345d334af98f3
Lokendra Chouhan <lokendra.chouhan@vulpinecode.com>
2018-03-14 18:04:30 +0530

    updated code

---
+++ common/models/traits/base/FollowerTrait.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\core\common\models\traits\base;
+
+// Yii Imports
+use Yii;
+
+// CMG Imports
+
+/**
+ * FollowerTrait can be used to add creator and modifier methods to relevant models.
+ *
+ * @property integer $createdBy
+ * @property integer $modifiedBy
+ *
+ * @since 1.0.0
+ */
+trait FollowerTrait {
+
+	// Variables ---------------------------------------------------
+
+	// Globals ----------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii classes ---------------------------
+
+	// CMG interfaces ------------------------
+
+	// CMG classes ---------------------------
+
+	// Validators ----------------------------
+
+	// AuthorTrait ---------------------------
+
+	
+
+	// Static Methods ----------------------------------------------
+
+	// Yii classes ---------------------------
+
+	// CMG classes ---------------------------
+
+	// AuthorTrait ---------------------------
+
+	// Read - Query -----------
+
+	// Read - Find ------------
+
+	// Create -----------------
+
+	// Update -----------------
+
+	// Delete -----------------
+
+}

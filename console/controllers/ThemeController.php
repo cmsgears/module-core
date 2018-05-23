fef4b1dbd8661de0d88fb85f1ec6706558932b8c
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-05-23 09:54:12 +0530

    Added theme controller to console app.

---
+++ console/controllers/ThemeController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\core\console\controllers;
+
+use yii\console\controllers\MigrateController;
+
+class ThemeController extends MigrateController {
+
+	// Make this theme as default theme.
+	public $default		= true;
+
+	// Activate this theme for current site.
+	public $activate	= true;
+
+	public function options( $actionID ) {
+
+		$options	= parent::options( $actionID );
+
+		$options[]	= 'default';
+		$options[]	= 'activate';
+
+		return $options;
+	}
+
+}

a01b09917d9abf19e1d505b9e7e1bd1caed46b10
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-07-27 17:40:49 +0530

    Applied quick fixes.

--- common/utilities/ContentUtil.php
+++ common/utilities/ContentUtil.php
@@ -63,6 +63,11 @@ class ContentUtil {
 
 	public static function findModel( $config ) {
 
+		if( empty( $config[ 'service' ] ) ) {
+
+			return;
+		}
+
 		$service	= Yii::$app->factory->get( $config[ 'service' ] );
 		$typed		= isset( $config[ 'typed' ] ) ? $config[ 'typed' ] : true;
 		$type		= isset( $config[ 'type' ] ) ? $config[ 'type' ] : $service->getParentType();
